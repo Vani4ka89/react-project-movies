dde9beda8aa82f16fe117dcf80f05aff61cfbf36
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-05-21 19:16:05 +0300

    master project 7

--- src/pages/GenrePage/GenrePage.tsx
+++ src/pages/GenrePage/GenrePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import css from './GenrePage.module.css';
+
 const GenrePage = () => {
     return (
-        <div>
+        <div className={css.box}>
             GenrePage
         </div>
     );
