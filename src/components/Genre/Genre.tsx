e49c942ffa120989d6551f88101281f0929c68d2
vania <ivan.tym4ak@gmail.com>
2023-08-20 23:51:55 +0300

    fixed some files

--- src/components/Genre/Genre.tsx
+++ src/components/Genre/Genre.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {NavLink, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 import {IGenre} from "../../interfaces";
 import '../../styles/components/Genre.css';
