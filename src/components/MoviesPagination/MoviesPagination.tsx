4dfedcb5c941a7cb102bf561157d1a7727f5495f
vania <ivan.tym4ak@gmail.com>
2023-08-21 10:33:00 +0300

    fixed some files

--- src/components/MoviesPagination/MoviesPagination.tsx
+++ src/components/MoviesPagination/MoviesPagination.tsx
@@ -10,35 +10,34 @@ const MoviesPagination = () => {
     const [query, setQuery] = useSearchParams();
 
     const currentPage = +query.get('page') ? +query.get('page') : 1;
-    const prevClass = `page-item ${page <= 1 ? 'disabled' : ''}`
+    const prevClass = `page-item ${page <= 1 ? 'disabled' : ''}`;
 
-    const pageLink = `/movies/?page=`;
+    const pageLink = `/movies?page=`;
     const currentPageLink = `${pageLink}${currentPage}`;
     const nextPageLink = `${pageLink}${currentPage + 1}`;
     const nextChildPageLink = `${pageLink}${currentPage + 2}`;
 
-
     const prevPage = (e: ReturnType<typeof Object>) => {
-        e.preventDefault()
+        e.preventDefault();
         if (currentPage <= 1) {
             return
         }
-        setQuery(prev => ({...prev, page: currentPage - 1}))
+        setQuery(prev => ({...prev, page: currentPage - 1}));
         window.scrollTo({top: 0, behavior: 'smooth'});
-    }
+    };
 
     const nextPage = (e: ReturnType<typeof Object>) => {
-        e.preventDefault()
-        setQuery(prev => ({...prev, page: currentPage + 1}))
+        e.preventDefault();
+        setQuery(prev => ({...prev, page: currentPage + 1}));
         window.scrollTo({top: 0, behavior: 'smooth'});
-    }
+    };
 
     return (
         <div>
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
                     <li className={prevClass}>
-                        <Link className="page-link" to={''} onClick={(e) => prevPage(e)} aria-label="Previous">
+                        <Link className="page-link" to={''} onClick={prevPage} aria-label="Previous">
                             <span aria-hidden="true">&laquo;</span>
                         </Link>
                     </li>
@@ -47,7 +46,7 @@ const MoviesPagination = () => {
                     <li className="page-item"><a className="page-link" href={nextChildPageLink}>{currentPage + 2}</a>
                     </li>
                     <li className="page-item">
-                        <Link className="page-link" to={''} onClick={(e) => nextPage(e)} aria-label="Next">
+                        <Link className="page-link" to={''} onClick={nextPage} aria-label="Next">
                             <span aria-hidden="true">&raquo;</span>
                         </Link>
                     </li>
